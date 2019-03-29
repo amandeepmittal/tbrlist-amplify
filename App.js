b4e5216228bd17bcc9e0e9dce9979f36063f9f5c
Aman Mittal <amandeepmittal@live.com>
2019-03-30 03:11:19 +0530

    mutation

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+import { API, graphqlOperation } from 'aws-amplify';
+
+const AddBook = `
+mutation ($title: String! $author: String) {
+  createBook(input: {
+    title: $title
+    author: $author
+  }) {
+    id title author
+  }
+}
+`;
+
 export default class App extends React.Component {
 	state = {
 		title: '',
@@ -10,6 +27,23 @@ export default class App extends React.Component {
 
 	onChangeText = (key, val) => {
 		this.setState({ [key]: val });
+	};
+
+	addBook = async () => {
+		if (this.state.title === '' || this.state.author === '') return;
+
+		const book = { title: this.state.title, author: this.state.author };
+
+		try {
+			const books = [...this.state.books, book];
+			this.setState({ books, title: '', author: '' });
+			console.log('books: ', books);
+
+			await API.graphql(graphqlOperation(AddBook, book));
+			console.log('success');
+		} catch (err) {
+			console.log('error: ', err);
+		}
 	};
 
 	render() {
@@ -27,11 +61,7 @@ export default class App extends React.Component {
 					onChangeText={val => this.onChangeText('author', val)}
 					placeholder="Who wrote it?"
 				/>
-				<Button
-					onPress={() => alert('Success!')}
-					title="Add to TBR"
-					color="#eeaa55"
-				/>
+				<Button onPress={this.addBook} title="Add to TBR" color="#eeaa55" />
 			</View>
 		);
 	}
