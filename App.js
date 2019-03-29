387def8ffe494d8ab07ed669c055836ff9e3c729
Aman Mittal <amandeepmittal@live.com>
2019-03-29 19:33:33 +0530

    add UI

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	state = {
+		title: '',
+		author: '',
+		books: []
+	};
+
+	onChangeText = (key, val) => {
+		this.setState({ [key]: val });
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<TextInput
+					style={styles.input}
+					value={this.state.title}
+					onChangeText={val => this.onChangeText('title', val)}
+					placeholder="What do you want to read?"
+				/>
+				<TextInput
+					style={styles.input}
+					value={this.state.author}
+					onChangeText={val => this.onChangeText('author', val)}
+					placeholder="Who wrote it?"
+				/>
+				<Button
+					onPress={() => alert('Success!')}
+					title="Add to TBR"
+					color="#eeaa55"
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		paddingHorizontal: 10,
+		paddingTop: 50
+	},
+	input: {
+		height: 50,
+		borderBottomWidth: 2,
+		borderBottomColor: 'blue',
+		marginVertical: 10
+	}
 });
